66c0049da4904278d49f8c9819641ce488eae336
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-08-24 13:49:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Initial value';
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'; 
+}
+setBestCustomer(bestCustomer);
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+overwriteBestCustomer(bestCustomer);
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'new value';
+}
+changeLeastFavoriteCustomer(leastFavoriteCustomer);
